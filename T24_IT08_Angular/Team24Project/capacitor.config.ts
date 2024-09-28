c2d1ec7fd69243b626ab54f1ebdfc9e08a8941f2
u20425644 <102963897+u20425644@users.noreply.github.com>
2024-09-28 21:31:59 +0200

    mobile app

---
+++ T24_IT08_Angular/Team24Project/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.bloomops.sunflowerguesthouse',
+  appName: 'Sunflower Guesthouse',
+  webDir: 'dist'
+};
+
+export default config;
