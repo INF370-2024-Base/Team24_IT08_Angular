d4c44299623953657fa6e1f3490cb0a2ed8330f5
u20425644 <102963897+u20425644@users.noreply.github.com>
2024-09-28 21:49:41 +0200

    Cap

--- T24_IT08_Angular/Team24Project/capacitor.config.ts
+++ T24_IT08_Angular/Team24Project/capacitor.config.ts
@@ -3,7 +3,9 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.bloomops.sunflowerguesthouse',
   appName: 'Sunflower Guesthouse',
-  webDir: 'dist'
+  webDir: 'dist/Team24Project',  // Replace Team24Project with the name of your Angular project
+  bundledWebRuntime: false
 };
 
 export default config;
+
