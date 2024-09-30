3ea4a6d0a04377bcc5d7a497814568f20de83c72
u20425644 <102963897+u20425644@users.noreply.github.com>
2024-09-30 21:13:06 +0200

    add

--- T24_IT08_Angular/Team24Project/src/environments/environments.ts
+++ T24_IT08_Angular/Team24Project/src/environments/environments.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://7e96-105-186-182-122.ngrok-free.app/swagger/index.html'
+  apiUrl: 'https://7e96-105-186-182-122.ngrok-free.app/swagger/index.html/'
 };
